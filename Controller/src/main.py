a84ec9153c8af65e0b8e5bd798edf883487a6e28
David Jander <djander@gmail.com>
2023-03-12 13:07:59 +0100

    Controller: main.py: Implement basic UI and frequency couter
    WIP
    
    Signed-off-by: David Jander <djander@gmail.com>

--- Controller/src/main.py
+++ Controller/src/main.py
@@ -1,12 +1,45 @@
 from machine import Pin, I2C, ADC
+import rp2
 from ssd1306 import SSD1306_I2C
 import framebuf
 import uasyncio as asyncio
 import sys
 import math
+from time import ticks_ms
 
 def adc2celsius(adc):
 	return (1 / (math.log(1/(65535/adc - 1))/3950 + 1/298.15) - 273.15)
+
+@rp2.asm_pio()
+def PIO_counter():
+	set(x, 0)
+	wrap_target()
+	label('inner')
+	wait(1, pin, 0)
+	wait(0, pin, 0)
+	jmp(x_dec, 'inner')
+	wrap()
+
+class FreqCounter:
+	def __init__(self, npin):
+		self.sm = rp2.StateMachine(0)
+		self.sm.init(PIO_counter, freq=125000000, in_base=Pin(npin, Pin.IN, Pin.PULL_UP))
+		self.sm.active(1)
+		self.ts0 = ticks_ms()
+		self.count0 = 0
+
+	def read(self):
+		self.sm.exec('mov(isr, x)')
+		self.sm.exec('push()')
+		count = (0x100000000 - self.sm.get()) & 0xffffffff
+		ts = ticks_ms()
+		dt = ts - self.ts0
+		dc = count - self.count0
+		self.ts0 = ts
+		self.count0 = count
+		if dc < 0 or dt <= 0:
+			return 0 # Invalid, retry
+		return (dc * 1000) / dt
 
 class UI:
 	maxlines = 4
@@ -14,10 +47,14 @@ class UI:
 	def __init__(self):
 		self.lines = ["" for i in range(self.maxlines)]
 		self.oled = SSD1306_I2C(128, self.maxlines * 8, I2C(0))
+		self.clear_buttons()
+
+	def clear_buttons(self):
+		self.btns = []
+		self.btn_selected = None
 
 	async def coro_init(self, loop):
 		self.loop = loop
-		self.text("Hello world", 0)
 		self.redraw()
 		self.loop.create_task(self.coro_keys())
 		print("Init")
@@ -47,13 +84,24 @@ class UI:
 			await asyncio.sleep(self.debounce)
 
 	def key_up(self, press):
-		print(f"Key UP {'pressed' if press else 'released'}")
+		#print(f"Key UP {'pressed' if press else 'released'}")
+		if self.btn_selected and press:
+			self.btn_selected = max(0, self.btn_selected - 1)
+			self.redraw()
 
 	def key_down(self, press):
-		print(f"Key Down {'pressed' if press else 'released'}")
+		#print(f"Key Down {'pressed' if press else 'released'}")
+		if self.btn_selected is not None and press:
+			self.btn_selected = min(self.btn_selected + 1, len(self.btns) - 1)
+			self.redraw()
 
 	def key_ok(self, press):
-		print(f"Key OK {'pressed' if press else 'released'}")
+		#print(f"Key OK {'pressed' if press else 'released'}")
+		if self.btn_selected is None or not press:
+			return
+		h = self.btns[self.btn_selected][3]
+		if h is not None:
+			h()
 
 	def key_cancel(self, press):
 		print(f"Key Cancel {'pressed' if press else 'released'}")
@@ -62,6 +110,13 @@ class UI:
 		self.lines[n] = s
 		if redraw:
 			self.redraw()
+
+	def button(self, x, y, s, handler):
+		self.btns.append([x, y, s, handler])
+		return len(self.btns) - 1
+
+	def set_button_text(self, idx, s):
+		self.btns[idx][2] = s
 
 	def clear(self):
 		self.oled.fill(0)
@@ -73,11 +128,62 @@ class UI:
 				self.oled.text(self.lines[i], 0, i * 8)
 			except IndexError:
 				break
+		for i, b in enumerate(self.btns):
+			x, y, s, _ = b
+			w = 8 * (len(s) + 1)
+			h = 8
+			self.oled.rect(x, y, w, h, 1, True)
+			self.oled.text(s, x + 4, y, 0)
+			if self.btn_selected == i:
+				for i in range(3):
+					self.oled.vline(x + i + 1, y + i + 1, 6 - i * 2, 0)
+					self.oled.vline(x + w - i - 2, y + i + 1, 6 - i * 2, 0)
 		self.oled.show()
 
 class Application:
 	def __init__(self):
 		self.ui = UI()
+		self.fc = FreqCounter(2)
+		self.temp_df = 0
+		self.temp_w = 0
+		self.flow_df = 0
+		self.pump_df_on = False
+		self.pump_w_on = False
+
+	def screen_main_setup(self):
+		self.ui.button(8, 0, "start", self.btn_start)
+		self.ui.button(80, 0, "stop", self.btn_stop)
+		self.ui.button(96, 8, "off", self.btn_pump_df)
+		self.ui.button(96, 16, "off", self.btn_pump_w)
+		self.ui.button(104, 24, "->", self.btn_next)
+		self.ui.btn_selected = 0
+
+	def screen_main_redraw(self):
+		self.ui.text(f"Df: {self.temp_df:4.1f} C", 1, False)
+		self.ui.text(f"W:  {self.temp_w:4.1f} C", 2, False)
+		self.ui.text(f"fr: {self.flow_df:4.1f} lpm", 3)
+
+	def btn_start(self):
+		print("Start system")
+
+	def btn_stop(self):
+		print("Stop system")
+
+	def btn_pump_df(self):
+		self.pump_df_on = not self.pump_df_on
+		self._set_btn_on_off(2, self.pump_df_on)
+
+	def btn_pump_w(self):
+		self.pump_w_on = not self.pump_w_on
+		self._set_btn_on_off(3, self.pump_w_on)
+
+	def btn_next(self):
+		print("Next screen")
+
+	def _set_btn_on_off(self, idx, val):
+		s = "on " if val else "off"
+		self.ui.set_button_text(idx, s)
+		self.ui.redraw()
 
 	def handle_exception(self, loop, ctx):
 		sys.print_exception(ctx["exception"])
@@ -90,14 +196,16 @@ class Application:
 		cnt = 0
 		adc = ADC(27)
 		acc = 0
+		flow = 0.0
+		self.screen_main_setup()
 		while True:
 			acc += adc2celsius(adc.read_u16())
 			cnt += 1
 			if cnt >= 10:
-				temp = acc / cnt
-				self.ui.text(f"Count {cnt}", 1, redraw=False)
-				self.ui.text(f"Temp: {temp:4.1f}", 2)
+				self.temp_df = acc / cnt
+				self.flow_df = self.fc.read() / 6.6
 				cnt = acc = 0
+				self.screen_main_redraw()
 			await asyncio.sleep(0.1)
 
 	def run(self):
